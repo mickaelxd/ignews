7437e08e9696838140e7f4f073b40ecae1c36b07
Mickael <mickael.n.xd@gmail.com>
2021-07-17 20:57:19 -0300

    Lógica de Redirecionamento para Checkout, Verificação para não duplicar usuário no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
+import { signIn, useSession } from 'next-auth/client';
 import React from 'react';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
 
 import styles from './styles.module.scss';
 
@@ -9,10 +12,32 @@ interface SubscribeButtonProps {
 export const SubscribeButton: React.FC<SubscribeButtonProps> = ({
   priceId,
 }) => {
+  const [session] = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import packageJson from '../../package.json';
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
     name: 'ignews',
